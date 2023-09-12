5b973e0fa1d5ad4076f26ffc8efbeddb76617815
Ilef01 <bchinyyilef@gmail.com>
2023-09-12 14:24:14 -0700

    e

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number
+ * Return: 1 if positive. 0 if zero. -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
