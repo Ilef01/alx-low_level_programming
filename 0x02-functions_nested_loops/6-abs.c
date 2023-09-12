922b70cdbc87b1a1ce7efe48b971108c8108f963
Ilef01 <bchinyyilef@gmail.com>
2023-09-12 14:55:45 -0700

    e

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - comptes the absolute value
+ * @n: the number
+ * Return: always 0 (success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+	return (0);
+}
