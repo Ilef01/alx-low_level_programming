855bcea4d596b2824862360ddfcf1fe99de2f2c2
Ilef01 <bchinyyilef@gmail.com>
2023-09-13 11:30:41 -0700

    a

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - Entry point
  * This function assigns a random number to n and detemines
  * whether its negative postive or zero
  *
  * It outputs a message
- * Return: Always 0 (success)
  */
 void positive_or_negative(int n)
 {
