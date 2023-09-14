626c3e95d91060749d454d6ddd6af5c6ad65ab8e
Ilef01 <bchinyyilef@gmail.com>
2023-09-14 13:21:56 -0700

    d

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - prints numberrs
+ * Return: always 0 (success)
+ */
+void print_numbers(void)
+{
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
