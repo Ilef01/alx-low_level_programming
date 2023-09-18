0f13ab40e66fecfafb6099c279f17671e79bae5e
Ilef01 <bchinyyilef@gmail.com>
2023-09-18 14:01:47 -0700

    b

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - updates pointer value
  * @n : the pointer
- * Return : always 0
+ * Return : void
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void more_numbers(void);
 void print_most_numbers(void);
