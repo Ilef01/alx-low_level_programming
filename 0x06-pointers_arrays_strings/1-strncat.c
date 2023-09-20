66f99173b69f887a382876cc54733fe688aaf779
Ilef01 <bchinyyilef@gmail.com>
2023-09-20 03:22:35 -0700

    b

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncat - returns length of a string
+ * @dest: the first string
+ * @src:the second string
+ * Return: the concatenation
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int i;
+
+	while (*dest != '\0')
+		dest++;
+	for (i = 0; i < n; i++)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (p);
+}
