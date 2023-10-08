b7a6f39d7d1259f69b010661581029d7c8185fcd
Ilef01 <bchinyyilef@gmail.com>
2023-10-08 06:02:35 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _memset - fills bytes of a memory with a constant
+ * @s: the pointer
+ * @b: the constant
+ * @n: the number of bytes
+ * Return: a pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+	return (s);
+}
+/**
  * *_calloc - allocates memory for an array
  * @nmemb: the number of elements
  * @size: the size of each element
@@ -14,5 +29,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	_memset(p, 0, nmemb * size);
 	return (p);
 }
