799ea3242ced13dd6bf9d4be4b529077a98d1dfc
Ilef01 <bchinyyilef@gmail.com>
2023-10-24 04:53:52 -0700

    betty fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,7 +5,7 @@
 /**
  * add_nodeint - adds a new node at the beginnig
  * @head: a pointer to the pointer to the head of the list
- * @int: the integer to be added
+ * @n: the integer to be added
  * Return: the address of the new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
