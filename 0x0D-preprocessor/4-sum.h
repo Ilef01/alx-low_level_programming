41d6a9ce6f0f125ef6ef99c907c5e61e0daba3b2
Ilef01 <bchinyyilef@gmail.com>
2023-10-25 08:34:02 -0700

    SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * SUM - Macro to sum 2 integers
+ * @x: first integer
+ * @y: second integer
+ * Return: the sum
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
