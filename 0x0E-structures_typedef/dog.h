cef97c7b2eb3eb62a4ee78f1734679d2364e43eb
Ilef01 <bchinyyilef@gmail.com>
2023-10-25 10:20:26 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Represents a dog.
+ * @name: Name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This structure defines a dog.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
