a9e365d8ce28828a18e968adb841ce2ac3f15b06
Ilef01 <bchinyyilef@gmail.com>
2023-10-25 12:41:01 -0700

    betty fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,18 +6,16 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
+ * Return: Pointer to the new structure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new = malloc(sizeof(dog_t));
 
 	if (new == NULL)
-		return(NULL);
-	else
-	{
-		new->name = name;
-		new->age = age;
-		new->owner = owner;
-	}
+		return (NULL);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
 	return (new);
 }
