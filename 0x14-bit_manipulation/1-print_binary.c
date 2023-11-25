9aebfd1ebc8044a4e2a822dc89ef4d91402b9966
Ilef01 <bchinyyilef@gmail.com>
2023-11-25 14:17:28 -0800

    printing the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,19 +6,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	int tmp = n;
-	int i = 0;
+	unsigned long int i = 0;
+	unsigned long int tmp = n;
 
 	while (tmp != 0)
 	{
 		tmp >>= 1;
 		i++;
 	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	while (i > 0)
 	{
 		_putchar((char)((n >> (i - 1)) & 1) + '0');
 		i--;
 	}
-	if (n == 0)
-		_putchar('0');
+
+
 }
