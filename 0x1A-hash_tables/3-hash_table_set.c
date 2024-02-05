ffc50c40c2bb7731f8dcd89d3fba22c80dee5717
Ilef01 <bchinyyilef@gmail.com>
2024-02-05 13:47:31 -0800

    compilation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * hash_table_set - adds an element to the hash table
  *
@@ -10,12 +11,16 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index(key, ht->size);
+	unsigned long int index;
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
 	if (node->key == NULL)
 	{
 		free(node);
